f905040a4074427a9fa11a0a230a7881ec223644
Mrinal Ghosh <mg712702@gmail.com>
2021-03-06 20:27:14 +0530

    ginkgo: Move back Thermal configs to vendor
    Signed-off-by: Mrinal Ghosh <mg712702@gmail.com>

--- device.mk
+++ device.mk
@@ -404,6 +404,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+<<<<<<< HEAD
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
     $(LOCAL_PATH)/configs/thermal/thermal-engine-camera.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-camera.conf \
